aebe7f9a6c5eef3afe1f051da1893e00e40d7058
Denilson 87 <denilsonmucavel@outlook.com>
2021-08-19 09:03:20 -0700

    First release

---
+++ tonight.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+<html>
+
+<?php
+   $mobilia=array(
+       array("Televisao",40, 10, 20),
+       array("Remote", 20, 20, 21),
+       array("Sofa", 29, 21, 12),
+       array("Computador", 20, 20, 20) 
+   );
+ for($row=0; $row<4; $row++){
+     echo "<p><b>Esta e a linha numero: $row</b></p>";
+     echo "<ul>";
+     for($col=0; $col<4; $col++){
+         echo "<li>".$mobilia[$row][$col]."</li>"; 
+         echo "</ul>";
+         
+                                
+                                }
+     
+     
+ }
+
+?>
+
+</html>
